67205a48110b1a2f6389bb79f1cebe27e6da5f36
Mocarram Hossain <mocarramhossain@gmail.com>
2026-06-20 03:28:30 +0600

    lekha 0.2.0

--- Casks/lekha.rb
+++ Casks/lekha.rb
@@ -1,9 +1,9 @@
 cask "lekha" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.1.1"
-  sha256 arm:   "52bbae08e1265ecbe73f35325671d1091247b371d47b2563b5f83ebc67dabbd6",
-         intel: "68fa38073680f269692a20eaebe7d4adb67811dd905b118a2c1aef2805c9b1f6"
+  version "0.2.0"
+  sha256 arm:   "3be7808b824204282f20699457e6fe62dda2324c6ae1a20611b3b2827c80d101",
+         intel: "fabfc9fcce3c162fc542b57f9e1149ed5fff1427c6df5366e02cf99e276ddd73"
 
   url "https://github.com/mocarram/Lekha/releases/download/v#{version}/Lekha-#{version}-#{arch}.dmg",
       verified: "github.com/mocarram/Lekha/"
